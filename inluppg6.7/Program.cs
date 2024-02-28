008c0598aa8e48ca1438586df6a7bd68dd4cccf0
kebabnekaise <emsi06001@utb.vaxjo.se>
2024-02-28 12:59:34 +0100

    Add project files.

---
+++ inluppg6.7/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace inluppg6_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv in ett tal");
+            int tal = int.Parse(Console.ReadLine());
+            if (isPrimtal(tal))
+            {
+                Console.WriteLine("Primtal");
+            }
+            else
+            {
+                Console.WriteLine("Inte primtal");
+            }
+        }
+
+        static bool isPrimtal(int x)
+        {
+            if(x % 2 == 0)
+            {
+                return false;
+            }
+
+            for (int i = 3; i < x; i += 2)
+            {
+                if (x % i == 0)
+                {
+                    return false;
+                }
+            }
+
+
+            return true;
+        }
+    }
+}
